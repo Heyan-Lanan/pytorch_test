57120d9a1ddb2c6c93b88c6dad1654f239dfb94e
Heyan-Lanan <2149730257@qq.com>
2022-11-11 23:12:53 +0800

    add

---
+++ day3/main.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import numpy as np
+print(np.__version__)
+print(torch.empty(5, 3))
+import PIL as p
+print(p.__version__)
--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
+import pandas as pd
+import torch
 
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
@@ -12,5 +13,7 @@ def print_hi(name):
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    print(pd.__version__)
+    print(torch.__version__)
+    print(torch.empty(5, 3))
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
